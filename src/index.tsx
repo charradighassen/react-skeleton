aa830f289ec3e33c70ea98338be8ca0617adfe5a
ghassen <ghassen@hitechnologies.com.br>
2022-11-15 11:47:41 +0100

    remove unused css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import "fontsource-roboto";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
